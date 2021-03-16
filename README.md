e65e0b8584dcd90c0ffbac81e1a0e5d71b9c27f3
Vahid Khorasani <v.khorasani@asanpardakht.ir>
2021-03-16 14:43:40 +0330

    Add cert_check script & Update README file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cert-check
+
+This script can be used to check your domain ssl certificate expiration time and let you now if you've already implemented a messaging system (such as Rocket.chat,mattermost,...).
+
+To use this script you should just run it by bash but before running make sure to set some variables based on your environment.
+
+Put your domain name in 'domain' variable and also set webhook and token based on your messaging system.
+
+After running this script it will create a json file which contains domain-name,start-date and expire-date and then tries to send it to your channel.
---
+++ cert_check.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+json_file="./body.json"
+bearer="YOUR_BEARER_TOKEN_SITS_HERE"
+webhook="YOUR_WEBHOOK_SITS_HERE"
+domain=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }'  | grep subject | cut -d"=" -f2)
+
+sday=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep -i "start date" | cut -d":" -f2 | cut -d" " -f3)
+smonth=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep -i "start date" | cut -d":" -f2 | cut -d" " -f2)
+syear=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep -i "start date" | cut -d":" -f4 | cut -d" " -f2)
+start=$(date -d"$sday $smonth $syear" +%Y-%m-%d)
+
+
+day=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep expire | cut -d":" -f2 | cut -d" " -f3)
+month=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep expire | cut -d":" -f2 | cut -d" " -f2)
+year=$(curl --insecure -vvI https://YOUR_DOMAIN_SITS_HERE 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep expire | cut -d":" -f4 | cut -d" " -f2)
+end=$(date -d"$day $month $year" +%Y-%m-%d)
+
+expire_date=$(date -d"$day $month $year" +%Y%m%d)
+
+now=$(date +%Y%m%d)
+let ddiff=(`date +%s -d $expire_date`-`date +%s -d $now`)/86400
+
+cat <<- EOF > $json_file
+    {
+    "domain" : "$domain",
+    "start_date" : "$start",
+    "end_date" : "$end"
+    }
+EOF
+
+curl -X POST -H "Content-Type: application/json" --data @body.json --header "authorization: Bearer $bearer" $webhook
